c3b584871cf8142de7544d579ea662c9c8395b85
naoya kinoshita <naoyakinoshita@naoyanoMacBook-Pro.local>
2020-04-08 13:52:41 +0900

    mod

--- with_timestamps/pipeline.py
+++ with_timestamps/pipeline.py
@@ -16,7 +16,7 @@ def run():
     (p
         | 'Garden plants' >> beam.Create([
             {'name': 'Strawberry', 'season': 1585699200}, # April, 2020
-            {'name': 'Carrot', 'season': 1590969600}     # June, 2020
+            {'name': 'Carrot', 'season': 1590969600},     # June, 2020
             {'name': 'Artichoke', 'season': 1583020800},  # March, 2020
             {'name': 'Tomato', 'season': 1588291200},     # May, 2020
             {'name': 'Potato', 'season': 1598918400},     # September, 2020
